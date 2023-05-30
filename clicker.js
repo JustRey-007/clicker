61203228742880f7df6984b71d909a1a5b4053f9
Andrew <93514394+JustRey-007@users.noreply.github.com>
2023-05-30 16:54:07 +0300

    Update clicker.js

--- clicker.js
+++ clicker.js
@@ -1,59 +1,67 @@
 let clicks = 1;
 
-const TIMEOUT = 10500;
-
+// объявление основных переменных
 const display = document.querySelector('#display');
 const button = document.querySelector('#button');
 const counter = document.querySelector('#counter');
 
 button.onclick = start;
 
-
+// функция клики
 function start() {
-    const startTime = Date.now();
+  button.onclick = () => {
+    counter.textContent = clicks++;
+    // достижение
+    if (clicks === 11) {
+      const image = document.createElement('img');
+      image.src = 'Досягнення.png'; // Замените на путь к вашему изображению
+      image.classList.add('animated-image');
+      display.appendChild(image);
 
-    display.textContent = formatTime(TIMEOUT);
-    button.onclick = () => counter.textContent = clicks++;
-
-    const interval = setInterval(() => {
-        const delta = Date.now() - startTime;
-        display.textContent = formatTime(TIMEOUT - delta);
-    }, 1);
-
-    const timeout = setTimeout(() => {
-        button.onclick = null;
-        display.textContent = 'Молодець!';
-
-        clearInterval(interval);
-        clearTimeout(timeout);
-    }, TIMEOUT);
+      setTimeout(() => {
+        image.classList.add('exit');
+        setTimeout(() => {
+          display.removeChild(image);
+        }, 2000); // Время задержки перед исчезновением (в миллисекундах)
+      }, 3000); // Время задержки перед запуском анимации исчезновения (в миллисекундах)
+    }
+  };
 }
 
-function formatTime(ms) {
-    return Number.parseFloat(ms / 1000).toFixed(2);
-}
+start();
 
-const showButton = document.getElementById('button');
-const invisibleButton = document.getElementById('myButton');
-
-showButton.addEventListener('click', () => {
-
-  invisibleButton.style.display = 'block';
-
-  showButton.classList.add('visible');
-});
-
-volumeControl = document.getElementById("volume");
-audio = document.getElementById("myAudio");
+// кнопка проигрыватель + ползунок громкости
+let audio = document.getElementById('audio');
+let playIcon = document.getElementById('playIcon');
+let volumeControl = document.getElementById('volumeControl');
+let soundContainer = document.getElementById('soundContainer');
+let isPlaying = false;
 
 function togglePlay() {
-  if (audio.paused) {
-    audio.play();
-  } else {
+  if (isPlaying) {
     audio.pause();
+    playIcon.src = 'no-sound-icon.png';
+    playIcon.alt = 'Pause Icon';
+  } else {
+    audio.play();
+    playIcon.src = 'sound-icon.png';
+    playIcon.alt = 'Play Icon';
   }
+
+  isPlaying = !isPlaying;
+}
+
+function showVolumeControl() {
+  volumeControl.style.display = 'block';
+}
+
+function hideVolumeControl() {
+  volumeControl.style.display = 'none';
 }
 
 function setVolume() {
   audio.volume = volumeControl.value;
 }
+
+soundContainer.addEventListener('mouseenter', showVolumeControl);
+soundContainer.addEventListener('mouseleave', hideVolumeControl);
