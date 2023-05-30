ece6ea90c2e339d5bc0097d528ad8acb51759bc0
Andrew <93514394+JustRey-007@users.noreply.github.com>
2023-05-30 22:39:20 +0300

    Update clicker.js

--- clicker.js
+++ clicker.js
@@ -9,23 +9,55 @@ button.onclick = start;
 
 // функция клики
 function start() {
+  const achievements = [
+    { clicksRequired: 11, imageSrc: 'Досягнення(1).png' },
+    { clicksRequired: 31, imageSrc: 'Досягнення(2).png' },
+    { clicksRequired: 101, imageSrc: 'Досягнення(3).png' },
+    { clicksRequired: 201, imageSrc: 'Досягнення(4).png' },
+    { clicksRequired: 401, imageSrc: 'Досягнення(5).png' },
+    { clicksRequired: 701, imageSrc: 'Досягнення(6).png' },
+    { clicksRequired: 1201, imageSrc: 'Досягнення(7).png' },
+    { clicksRequired: 2001, imageSrc: 'Досягнення(8).png' },
+    { clicksRequired: 4001, imageSrc: 'Досягнення(9).png' },
+    { clicksRequired: 10001, imageSrc: 'Досягнення(10).png' },
+    // Добавьте остальные достижения в формате { clicksRequired: <количество кликов>, imageSrc: '<путь к изображению>' }
+  ];
+
+  const achievedAchievements = [];
+
   button.onclick = () => {
     counter.textContent = clicks++;
-    // достижение
-    if (clicks === 11) {
-      const image = document.createElement('img');
-      image.src = 'Досягнення.png'; // Замените на путь к вашему изображению
-      image.classList.add('animated-image');
-      display.appendChild(image);
 
-      setTimeout(() => {
-        image.classList.add('exit');
-        setTimeout(() => {
-          display.removeChild(image);
-        }, 2000); // Время задержки перед исчезновением (в миллисекундах)
-      }, 3000); // Время задержки перед запуском анимации исчезновения (в миллисекундах)
-    }
+    // Проверка достижений
+    achievements.forEach((achievement) => {
+      if (clicks === achievement.clicksRequired && !achievedAchievements.includes(achievement)) {
+        addToAchievements(achievement);
+        achievedAchievements.push(achievement);
+      }
+    });
   };
+}
+
+function addToAchievements(achievement) {
+  const image = document.createElement('img');
+  image.src = achievement.imageSrc;
+  image.classList.add('animated-image');
+  display.appendChild(image);
+
+  setTimeout(() => {
+    image.classList.add('exit');
+    setTimeout(() => {
+      display.removeChild(image);
+    }, 2000); // Время задержки перед исчезновением (в миллисекундах)
+  }, 3000); // Время задержки перед запуском анимации исчезновения (в миллисекундах)
+
+  // Добавление изображения в список достижений
+  const achievementsList = document.getElementById('achievements-list');
+  const listItem = document.createElement('li');
+  const listItemImage = document.createElement('img');
+  listItemImage.src = achievement.imageSrc;
+  listItem.appendChild(listItemImage);
+  achievementsList.appendChild(listItem);
 }
 
 start();
